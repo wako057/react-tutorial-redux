2286ba1c71df6063f19407cae198e53284918780
Gregory Gerard <gregory@flatchr.io>
2024-02-05 09:39:13 +0100

    feat: a more realistic use case

--- redux-demo.js
+++ redux-demo.js
@@ -1,10 +1,18 @@
 const redux = require('redux');
 
 const counterReducer = (state = { counter: 0 }, action) => {
-	console.log(action);
-	return {
-		counter: state.counter + 1
-	};
+	console.log(state, action);
+	if (action.type === 'increment') {
+		return {
+			counter: state.counter + 1
+		};
+	}
+	if (action.type === 'decrement') {
+		return {
+			counter: state.counter - 1
+		};
+	}
+	return state
 };
 
 const store = redux.createStore(counterReducer);
@@ -19,3 +27,4 @@ const counterSubscriber = () => {
 store.subscribe(counterSubscriber);
 
 store.dispatch({ type: 'increment' });
+store.dispatch({ type: 'decrement' });
