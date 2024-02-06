03c8795e836d86e0a53904810970078d4bd5c3da
Gregory Gerard <gregory@flatchr.io>
2024-02-06 10:13:36 +0100

    feat: connect action to redux reducer

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,8 +1,17 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  const dispatch = useDispatch();
   const counter = useSelector(state => state.counter);
+
+  function handleIncrement() {
+    dispatch({type: 'increment'})
+  }
+  function handleDecrement() {
+    dispatch({type: 'decrement'})
+  }
+
 
   const toggleCounterHandler = () => {};
 
@@ -10,6 +19,10 @@ const Counter = () => {
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={handleIncrement}>Increment</button>
+        <button onClick={handleDecrement}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
